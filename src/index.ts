6ebbcc9459c0dbe44a589a43db05f0d403a06398
Julia Khvatova <jkhvatova@gmail.com>
2024-02-11 22:52:04 +0300

    feat: add uuid and POST method

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-//import { UUID, randomUUID } from 'crypto';
-import { v4 as uuid } from "uuid";
+
+import { v4 as uuidv4 } from 'uuid';
 import { createServer, IncomingMessage, Server, ServerResponse } from 'http';
 // In-memory database
 type User = {
-  id: string | typeof uuid,
+  id: string,
   username: string,
   age: number,
   hobbies: Array<string>,
@@ -20,10 +20,10 @@ console.log(ENDPOINTS.user.split('/')[3]);
 
 // create server
 const server: Server = createServer((req: IncomingMessage, res: ServerResponse) => {
-  if (req.url === '/api/users' && req.method === 'GET') {
+  if (req.url === ENDPOINTS.users && req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(USERS_DB));
-  } else if (req.url?.startsWith('/api/users/') && req.method === 'GET') {
+  } else if (req.url?.startsWith(ENDPOINTS.users) && req.method === 'GET') {
     const id: string = req.url.split('/')[3];
     const user = USERS_DB.find(user => user.id === id);
     if (user) {
@@ -31,12 +31,36 @@ const server: Server = createServer((req: IncomingMessage, res: ServerResponse) 
       res.end(JSON.stringify(user));
     } else {
       res.writeHead(404, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ message: 'user not found' }));
+      res.end(JSON.stringify({ message: 'Oops, I cannot find that user:(' }));
     }
+  } else if (req.url === ENDPOINTS.users && req.method === 'POST') {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk;
+    });
+    req.on('end', () => {
+      const newUserData: Partial<User> = JSON.parse(body);
+      if (!newUserData.username || !newUserData.age || !newUserData.hobbies) {
+        res.writeHead(400, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Oops! Request body must contain username, age, and hobbies' }));
+      } else {
+        newUserData.id = uuidv4();
+        const newUser = {
+          id: newUserData.id,
+          username: newUserData.username,
+          age: newUserData.age,
+          hobbies: newUserData.hobbies
+        }
+        USERS_DB.push(newUser);
+        res.writeHead(201, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(newUser));
+      }
+
+    });
   }
   else {
     res.writeHead(404, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ message: 'Route not found' }));
+    res.end(JSON.stringify({ message: 'Oops, I cannot find that route:(' }));
   }
 });
 
